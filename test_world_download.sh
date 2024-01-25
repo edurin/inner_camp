5a456cd8947eb91a096cd469fe4c71af328b78b6
Eduard Kuznetsov <e.kuznetsov@gym2.nsk.ru>
2024-01-25 17:23:03 +0700

    MINOR

--- test_world_download.sh
+++ test_world_download.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #gittest
-git clone https://github.com/edurin/educational_worlds.git
-cd educational_worlds/
+git clone https://github.com/edurin/inner_camp.git
+cd inner_camp/
 cp -rf models/ /home/clover/.gazebo/
 cp -f simulator.launch /home/clover/catkin_ws/src/clover/clover_simulation/launch/
 cp -f parquet.jpg /home/clover/catkin_ws/src/clover/clover_simulation/models/parquet_plane/materials/textures/
 cp -f shirokaya_1.world /home/clover/catkin_ws/src/clover/clover_simulation/resources/worlds/
 cd ..
-rm -rf educational_worlds/
+rm -rf inner_camp/
 echo "Done!"
